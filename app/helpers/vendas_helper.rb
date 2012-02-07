8060a281d0d878e765e9813f87de1dccf632fc46
Igor Batista <igor.batista@gmail.com>
2012-02-07 07:57:49 -0300

    mudar venda diaria para find com where

--- app/helpers/vendas_helper.rb
+++ app/helpers/vendas_helper.rb
@@ -1,7 +1,8 @@
 module VendasHelper
   def qtde_total_diaria
     total = 0
-    vendas = Venda.venda_diaria
+    #vendas = Venda.venda_diaria
+    vendas = Venda.where('DATE(created_at) = ?',Date.today).order('created_at')
     vendas.each do |venda|
       venda.items.to_a.sum do |item|
         total += item.qtde
@@ -12,7 +13,8 @@ module VendasHelper
 
   def qtde_total_diaria_recebido
     total = 0
-    vendas = Venda.venda_diaria.where(:pago => true)	
+    #vendas = Venda.venda_diaria.where(:pago => true)	
+    vendas = Venda.where('DATE(created_at) = ? and pago = ?',Date.today, true).order('created_at')
     vendas.each do |venda|
       venda.items.to_a.sum do |item|
         total += item.qtde
